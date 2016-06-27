5eac76bc1e9892c4deda21f039ca2417de95898a
Gary <gary.brandon@gmail.com>
2016-06-27 20:13:54 +1000

    Fixing up initial model for posts.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    date_add = models.DateTimeField('Date added', auto_now=True)
+    date_pub = models.DateTimeField('Date published', null=True, blank=True)
