70891aaafa7d73cb91cd0ecb3cc91b6db691aab2
Gary <gary.brandon@gmail.com>
2016-06-27 20:37:06 +1000

    Created base template - currently in both blog and posts for testing. Updated views to do something a little more useful. Currently it'll display blog posts.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Posts
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    posts = Posts.objects.order_by('-date_pub')[:5]
+    context = {'posts': posts}
+    return render(request, 'posts/base.html', context)
+
+
+def post_view(request, post_id):
+    post = get_object_or_404(Posts, pk=post_id)
+    context = {'post': post}
+    return render(request, 'posts/base.html', context)
