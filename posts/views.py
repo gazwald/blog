8fc56875ad79cf40b1b72365e285bf25a53bf37e
Gary <gary.brandon@gmail.com>
2016-06-30 21:02:33 +1000

    I'm an idiot.

--- posts/views.py
+++ posts/views.py
@@ -5,16 +5,16 @@ from .models import Posts
 def index(request):
     posts = Posts.objects.order_by('-date_pub')[:5]
     context = {'posts': posts}
-    return render(request, 'posts/base.html', context)
+    return render(request, 'posts/view.html', context)
 
 
 def post_view(request, post_id):
     post = get_object_or_404(Posts, pk=post_id)
     context = {'post': post}
-    return render(request, 'posts/base.html', context)
+    return render(request, 'posts/view.html', context)
 
 
 def post_add(request):
     post = get_object_or_404(Posts, pk=post_id)
     context = {'post': post}
-    return render(request, 'posts/base.html', context)
+    return render(request, 'posts/view.html', context)
