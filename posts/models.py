a6788c0b36878c9244d321a09a0d04f6e159c974
Gary <gary.brandon@gmail.com>
2016-07-04 20:56:53 +1000

    Getting everything set for pushing to github.

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Posts(models.Model):
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=100)
     body = models.TextField()
     date_add = models.DateTimeField('Date added', auto_now=True)
     date_pub = models.DateTimeField('Date published', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
