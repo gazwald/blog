58a5a83ade88789c290b4288f4f44dbe35d6cd04
Gazwald <sysadmin@gazwald.com>
2016-07-06 20:05:58 +1000

    Renaming Posts to Post.

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Posts(models.Model):
+class Post(models.Model):
     author = models.ForeignKey(User)
     title = models.CharField(max_length=100)
     body = models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Posts
+from .models import Post
 
 
 def index(request):
