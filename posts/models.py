b35221a7f7128db984bc74f173cf01ddf4e4f02b
Gazwald <sysadmin@gazwald.com>
2016-07-06 21:03:39 +1000

    Added Category for Post

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
+
 class Post(models.Model):
     author = models.ForeignKey(User)
+    category = models.ForeignKey(Category, null=True)
     title = models.CharField(max_length=100)
     body = models.TextField()
     date_add = models.DateTimeField('Date added', auto_now=True)
--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 
 
 def index(request):
-    posts = Post.objects.order_by('-date_pub')[:5]
+    category = get_object_or_404(Category, category='post')
+    posts = Post.objects.order_by('-date_pub').filter(category=category)[:5]
     context = {'posts': posts}
     return render(request, 'posts/view.html', context)
 
 
 def post_view(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    context = {'post': post}
-    return render(request, 'posts/view.html', context)
-
-
-def post_add(request):
     post = get_object_or_404(Post, pk=post_id)
     context = {'post': post}
     return render(request, 'posts/view.html', context)
