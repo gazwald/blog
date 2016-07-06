0dcd2e8caa5d97945a7f88a406e5ed3dd08a9584
Gazwald <sysadmin@gazwald.com>
2016-07-06 20:18:39 +1000

    Removed last of Posts references. Adjusting column formatting for templates.

--- posts/views.py
+++ posts/views.py
@@ -3,18 +3,18 @@ from .models import Post
 
 
 def index(request):
-    posts = Posts.objects.order_by('-date_pub')[:5]
+    posts = Post.objects.order_by('-date_pub')[:5]
     context = {'posts': posts}
     return render(request, 'posts/view.html', context)
 
 
 def post_view(request, post_id):
-    post = get_object_or_404(Posts, pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     context = {'post': post}
     return render(request, 'posts/view.html', context)
 
 
 def post_add(request):
-    post = get_object_or_404(Posts, pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     context = {'post': post}
     return render(request, 'posts/view.html', context)
