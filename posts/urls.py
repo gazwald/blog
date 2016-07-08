9ff8dfe06b2e8350b16a112699eda4951123e66e
Gazwald <sysadmin@gazwald.com>
2016-07-08 20:40:52 +1000

    Basic view for adding a post

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^add/$', views.post_add, name='post_add'),
     url(r'^(?P<post_id>[0-9]+)$', views.post_view, name='post_view'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def index(request):
@@ -23,3 +26,16 @@ def post_view(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     context = {'post': post}
     return render(request, 'posts/view.html', context)
+
+
+@login_required
+def post_add(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+
+    form = PostForm(initial={'date_pub': timezone.now()})
+    context = {'form': form}
+    return render(request, 'posts/add.html', context)
