7418201a66bdac41a0c6827741f4edc06e65d1bf
Gazwald <me@gazwald.com>
2016-07-20 21:49:40 +1000

    Starting initial work on writing unittests. How useful these will be later on is yet to be seen.

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
 
-# Create your tests here.
+from .views import post_add
+from .models import Post
+
+class TestPosts(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+                username="my_user",
+                password="ALackOfSecurity",
+                email="root@localhost")
+
+    def test_add_post(self):
+        new_post = Post(
+                    author=self.user,
+                    title="Hello world",
+                    body="I am a test post, isn't this great")
+        self.assertEqual(new_post.title, "Hello world")
+
+
+    def test_add_post_logged_in(self):
+        request = self.factory.get('/posts/add/')
+        request.user = self.user
+        response = post_add(request)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_add_post_anonymous(self):
+        request = self.factory.get('/posts/add/')
+        request.user = AnonymousUser()
+        response = post_add(request)
+        self.assertEqual(response.status_code, 302)
