b3556624605526e6c3f312fcacf1223f8ba11839
Gazwald <me@gazwald.com>
2016-09-03 14:53:57 +1000

    Adding unit tests for posts and about

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
 
-# Create your tests here.
+from .views import index
+
+
+class TestPosts(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+                username="my_user",
+                password="ALackOfSecurity",
+                email="root@localhost.localdomain")
+
+
+    def test_view_about_user(self):
+       request = self.factory.get('/posts/')
+       request.user = self.user
+       response = index(request)
+       self.assertEqual(response.status_code, 200)
+
+
+    def test_view_about_anon(self):
+       request = self.factory.get('/posts/')
+       request.user = AnonymousUser()
+       response = index(request)
+       self.assertEqual(response.status_code, 200)
--- posts/tests.py
+++ posts/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser, User
+from django.utils import timezone
 
-from .views import index, post_add
-from .models import Post
+from .views import index, post_add, post_view
+from .models import Post, Category
+from .forms import PostForm
 
 
 class TestPosts(TestCase):
@@ -11,12 +13,34 @@ class TestPosts(TestCase):
         self.user = User.objects.create_user(
                 username="my_user",
                 password="ALackOfSecurity",
-                email="root@localhost")
+                email="root@localhost.localdomain")
+
+    def generic_post(self):
+        return Post.objects.create( author=self.user,
+                                    title='A Title',
+                                    body='A Body',
+                                    date_pub = timezone.now())
+
+    def test_category_creation(self):
+        new_cat = Category.objects.create(category='My Category')
+        self.assertIsInstance(new_cat, Category)
+
+
+    def test_post_creation(self):
+        new_post = self.generic_post()
+        self.assertIsInstance(new_post, Post)
 
 
     def test_view_post_index(self):
        request = self.factory.get('/posts/')
        response = index(request)
+       self.assertEqual(response.status_code, 200)
+
+
+    def test_view_post_id(self):
+       new_post = self.generic_post()
+       request = self.factory.get('/posts/1')
+       response = post_view(request, 1)
        self.assertEqual(response.status_code, 200)
 
 
@@ -30,6 +54,14 @@ class TestPosts(TestCase):
 
     def test_add_post_logged_in(self):
         request = self.factory.get('/posts/add/')
+        request.user = self.user
+        response = post_add(request)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_create_valid_post_logged_in(self):
+        data = {'author': 1, 'title': 'Hello World', 'body': 'Another amazing post'}
+        request = self.factory.post('/posts/add/', data)
         request.user = self.user
         response = post_add(request)
         self.assertEqual(response.status_code, 200)
