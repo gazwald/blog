2a63ce81e6d48a93be69d5c028ebb4b79700e257
Gazwald <me@gazwald.com>
2016-09-03 15:45:44 +1000

    Slug was meant to have unique=True, not Title

--- posts/models.py
+++ posts/models.py
@@ -11,8 +11,8 @@ class Category(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(User)
     category = models.ForeignKey(Category, null=True)
-    title = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField()
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
     body = models.TextField()
     date_add = models.DateTimeField('Date added', auto_now=True)
     date_pub = models.DateTimeField('Date published', null=True, blank=True)
