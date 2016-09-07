bd0f606a400a2940ee6de11b8fcb14dd15f091b9
Gazwald <me@gazwald.com>
2016-09-07 21:18:36 +1000

    Messing around with ElasticBeanStalk

--- blog/settings.py
+++ blog/settings.py
@@ -77,8 +77,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-if os.getenv('ENVIRONMENT') is 'PROD':
-    pass
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 else:
     DATABASES = {
         'default': {
